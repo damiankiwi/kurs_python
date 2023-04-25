73415af9fde32135aee4403f770780c6d000e2f0
damiankiwi <131194887+damiankiwi@users.noreply.github.com>
2023-04-25 20:13:05 +0200

    Delete Stringi_zad_4.py

--- 01_zadania_domowe/Stringi_zad_4.py
+++
@@ -1,7 +0,0 @@
-"""Utwórz skrypt, który zapyta użytkownika o tytuł książki,
- nazwisko autora, liczbę stron, a następnie:"""
-
-tytul = input("Podaj tytuł książki ->")
-autor = input("Podaj nazwisko autora ->")
-ilosc = input("Podaj ilość stron ->")
-
