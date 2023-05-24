5b6782d1cf68583d541859847312e113b553afa8
damiankiwi <kosipraca@gmail.com>
2023-05-24 14:11:54 +0200

    add files 06

--- 06_operations_on_files/05.py
+++ 06_operations_on_files/05.py
@@ -20,3 +20,19 @@ text = get_file()
 search_word = find_longest_word(text)
 
 print("Najdłuższe słowo w pliku to:",search_word, ', o długości:', len(search_word))
+
+# filename = "./intro/pan_tadzio.txt"
+#
+# with open(filename) as fopen:
+#     content = fopen.read()
+#
+# content = content.replace(',', '').split()
+#
+# longest_word = ''
+# for word in content:
+#     if len(word) > len(longest_word):
+#         print(word, '-', longest_word)
+#         print(len(word),'-',  len(longest_word))
+#         longest_word = word
+#
+# print("Najdłuże słowo to ... ", longest_word)
