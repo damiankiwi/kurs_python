a56c13f1bfa879ef95b8308929af24f2fd55fdc1
damiankiwi <kosipraca@gmail.com>
2023-05-24 13:57:42 +0200

    add operations_on_files

--- 06_operations_on_files/02.py
+++ 06_operations_on_files/02.py
@@ -3,25 +3,15 @@ import time
 
 quotes_file = "quotes.txt"
 
-# Pozyskiwanie bieżącej ścieżki dostępu za pomocą funkcji os.getcwd:
+# ścieżka dostępu:
 print(os.getcwd())
 
-#Zmiana ścieżki dostępu za pomocą funkcji os.chdir:
-os.chdir("C:/")
-print(os.getcwd())
-
-# Uzyskanie listy plików i folderów zawartych w danej lokalizacji za pomocą funkcji os.listdir:
+# lista plików i folderów zawartych w danej lokalizacji:
 l = list(os.listdir())
 print(l)
 
-# Pozyskanie rozmiaru pliku w bajtach za pomocą funkcji os.path.getsize:
+# rozmiaru pliku w bajtach:
 print(f'Rozmiar pliku {quotes_file} wynosi {os.path.getsize("quotes.txt")} bajtów')
 
-# Pozyskanie daty utworzenia pliku za pomocą funkcji os.path.getctime:
+# data utworzenia pliku:
 print(time.ctime(os.path.getctime(quotes_file)))
-
-# Usuwanie pliku za pomocą funkcji os.remove:
-with open("plik.txt", "w", encoding='utf-8') as fp:
-    fp.write("jakiś tam tekst")
-
-os.remove("plik.txt")
