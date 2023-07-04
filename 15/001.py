c99c75747691f993d45ab93742a0503f054ea5e5
damiankiwi <kosipraca@gmail.com>
2023-07-04 10:46:39 +0200

    add file

---
+++ 15/001.py
@@ -0,0 +1,8 @@
+liczba1 = float(input("Podaj pierwszą liczbę: "))
+liczba2 = float(input("Podaj drugą liczbę: "))
+
+if liczba1 > 0 and liczba2 > 0:
+    wynik = liczba1 * liczba2
+    print("Wynik mnożenia: ", wynik)
+else:
+    print("Nie mnożę!")
