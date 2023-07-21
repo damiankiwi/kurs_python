990cfc643c30f1b2e837ddd6b36445ecd20b2f2f
damiankiwi <kosipraca@gmail.com>
2023-07-21 22:48:48 +0200

    add basic

---
+++ 17/01.py
@@ -0,0 +1,4 @@
+def twinkle_star():
+    print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+twinkle_star()
