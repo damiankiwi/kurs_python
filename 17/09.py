44e9b0fd2ad82f29cbf4d7244613f4152aa35edb
damiankiwi <kosipraca@gmail.com>
2023-07-26 21:30:10 +0200

    add basic

---
+++ 17/09.py
@@ -0,0 +1,9 @@
+def main():
+    exam_st_date = (27, 7, 2023)
+
+    dzien, miesiac, rok = exam_st_date
+
+    print("Egzamin rozpocznie się: {} / {} / {}".format(dzien, miesiac, rok))
+
+if __name__ == "__main__":
+    main()
