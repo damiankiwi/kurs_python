39ae48842c354ba1ae545feb6a23e8ae6be3a91e
damiankiwi <kosipraca@gmail.com>
2023-07-26 21:27:32 +0200

    add basic

---
+++ 17/08.py
@@ -0,0 +1,8 @@
+def main():
+    color_list = ["Red", "Green", "White", "Black"]
+    print("Pierwszy kolor:", color_list[0])
+    print("Ostatni kolor:", color_list[-1])
+
+
+if __name__ == "__main__":
+    main()
