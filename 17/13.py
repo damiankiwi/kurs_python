966f53eaa58e64ea2859fbe794ea17fae054883d
damiankiwi <kosipraca@gmail.com>
2023-07-29 23:06:03 +0200

    add basic

---
+++ 17/13.py
@@ -0,0 +1,10 @@
+def main():
+    tekst = '''a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example'''
+
+    print(tekst)
+
+if __name__ == "__main__":
+    main()
