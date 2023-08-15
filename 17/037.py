7407e67a0a0ca5c7fd1a9caefcaac3d50b3a5fc1
damiankiwi <kosipraca@gmail.com>
2023-08-15 15:22:13 +0200

    my first repository

---
+++ 17/037.py
@@ -0,0 +1,10 @@
+def wypisz_dane(imie, wiek, adres):
+    print(f"Imię: {imie}")
+    print(f"Wiek: {wiek}")
+    print(f"Adres: {adres}")
+
+imie = input("Podaj imię: ")
+wiek = input("Podaj wiek: ")
+adres = input("Podaj adres: ")
+
+wypisz_dane(imie, wiek, adres)
