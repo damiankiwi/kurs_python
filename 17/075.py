0dcf6e8eb92aa41ac5451d89cfa73a29f4732945
damiankiwi <kosipraca@gmail.com>
2023-09-11 21:05:16 +0200

    add basic

---
+++ 17/075.py
@@ -0,0 +1,10 @@
+#CCopyright (C) 2023 Twoja Firma
+#Ten program jest chroniony prawem autorskim.
+#Wszelkie prawa zastrzeżone.
+
+def main():
+#Tutaj znajduje się kod programu
+    pass
+
+if __name__ == "__main__":
+    main()
