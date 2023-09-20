e7c9f89a6f9dd3defff591df1b24ce0335802168
damiankiwi <kosipraca@gmail.com>
2023-09-20 21:14:25 +0200

    add basic

---
+++ 17/092.py
@@ -0,0 +1,8 @@
+
+tekst = "Ten łańcuch zawiera znaki specjalne: \n"
+tekst += "Backslash: \\ \n"
+tekst += "Apostrof (pojedynczy cudzysłów): \' \n"
+tekst += "Cudzysłów (podwójny cudzysłów): \" \n"
+tekst += "Tabulacja: \t Teks taki"
+
+print(tekst)
