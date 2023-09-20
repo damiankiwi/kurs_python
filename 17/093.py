3fa62c613ff3c52aefb66e249718aff83b4fd9ca
damiankiwi <kosipraca@gmail.com>
2023-09-20 21:15:33 +0200

    add basic

---
+++ 17/093.py
@@ -0,0 +1,12 @@
+
+obiekt = 42
+
+tozsamosc = id(obiekt)
+
+typ = type(obiekt)
+
+wartosc = obiekt
+
+print("Tożsamość obiektu:", tozsamosc)
+print("Typ obiektu:", typ)
+print("Wartość obiektu:", wartosc)
