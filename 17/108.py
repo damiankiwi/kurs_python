2c189686579610710c18a24d762b0ceed6ba9759
damiankiwi <kosipraca@gmail.com>
2023-09-28 23:36:11 +0200

    add basic

---
+++ 17/108.py
@@ -0,0 +1,11 @@
+import os
+
+nazwa_sciezki = "108.py"
+
+for root, dirs, files in os.walk("/"):
+    if nazwa_sciezki in files or nazwa_sciezki in dirs:
+        pelna_sciezka = os.path.join(root, nazwa_sciezki)
+        print("Znaleziona ścieżka:", pelna_sciezka)
+        break
+else:
+    print("Nie znaleziono ścieżki o nazwie:", nazwa_sciezki)
