1f15dbb38819a6995883a175d0ea8e1bca523d91
damiankiwi <kosipraca@gmail.com>
2023-10-01 13:01:59 +0200

    add basic

---
+++ 17/112.py
@@ -0,0 +1,11 @@
+
+moja_lista = [1, 2, 3, 4, 5]
+
+
+if len(moja_lista) > 0:
+    moja_lista.pop(0)
+    print("Pierwszy element został usunięty.")
+else:
+    print("Lista jest pusta.")
+
+print("Aktualna lista:", moja_lista)
