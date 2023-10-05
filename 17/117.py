544a82e74f309f27858a148ab471204b85ea85a0
damiankiwi <kosipraca@gmail.com>
2023-10-05 17:16:21 +0200

    add basic

---
+++ 17/117.py
@@ -0,0 +1,8 @@
+
+string1 = "Hello, world!"
+string2 = "Hello, world!"
+
+if id(string1) == id(string2):
+    print("Obie zmienne wskazują na ten sam obszar pamięci.")
+else:
+    print("Zmienne wskazują na różne obszary pamięci.")
