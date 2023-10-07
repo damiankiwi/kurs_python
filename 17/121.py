2ec74cbbc5dbce65ec608159d0e052651ffe068f
damiankiwi <kosipraca@gmail.com>
2023-10-07 09:47:56 +0200

    add basic

---
+++ 17/121.py
@@ -0,0 +1,5 @@
+
+if 'moja_zmienna' in locals() or 'moja_zmienna' in globals():
+    print("Zmienna 'moja_zmienna' jest zdefiniowana.")
+else:
+    print("Zmienna 'moja_zmienna' nie jest zdefiniowana.")
