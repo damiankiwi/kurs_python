c05bd805fac1c6cb7742aa3213bf553486292f9d
damiankiwi <kosipraca@gmail.com>
2023-10-08 23:53:22 +0200

    add basic

---
+++ 17/122.py
@@ -0,0 +1,15 @@
+
+n = int(input("Podaj wartość zmiennej n: "))
+
+print("Początkowa wartość zmiennej n:", n)
+
+# Opróżnij zmienną n
+n = 0
+
+print("Opróżniono zmienną n. Nowa wartość:", n)
+
+d = {"x": 200}
+
+d.clear()
+
+print("Opróżniony słownik d:", d)
