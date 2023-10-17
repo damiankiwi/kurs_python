d6d3156560e32e3614ac00119038494a4182a78a
damiankiwi <kosipraca@gmail.com>
2023-10-17 21:18:52 +0200

    add basic

---
+++ 17/138.py
@@ -0,0 +1,10 @@
+
+prawda = True
+falsz = False
+
+wartosc_liczbowa_prawda = int(prawda)
+wartosc_liczbowa_falsz = int(falsz)
+
+# Wyświetlenie wyników
+print(f'Wartość logiczna True jako liczba całkowita: {wartosc_liczbowa_prawda}')
+print(f'Wartość logiczna False jako liczba całkowita: {wartosc_liczbowa_falsz}')
