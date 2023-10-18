87dc670928747c84f63ebf8a4a82da2e7689cebc
damiankiwi <kosipraca@gmail.com>
2023-10-18 21:28:24 +0200

    add basic

---
+++ 17/141.py
@@ -0,0 +1,12 @@
+def dziesietna_na_szesnastkowa(liczba):
+    szesnastkowa = hex(liczba)
+    return szesnastkowa
+
+liczba1 = 30
+liczba2 = 4
+
+szesnastkowa1 = dziesietna_na_szesnastkowa(liczba1)
+szesnastkowa2 = dziesietna_na_szesnastkowa(liczba2)
+
+print(szesnastkowa1)
+print(szesnastkowa2)
