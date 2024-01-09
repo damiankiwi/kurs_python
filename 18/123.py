a23ba702c9dc54a5d5d4397f14ceb59e1c1f570b
damiankiwi <kosipraca@gmail.com>
2024-01-09 23:25:13 +0100

    add basic

---
+++ 18/123.py
@@ -0,0 +1,11 @@
+def usun_pierwszy_i_ostatni(ciag):
+    return ciag[1:-1]
+
+print("Oryginalny ciąg: PHP")
+print("Usunięcie pierwszego i ostatniego elementu:", usun_pierwszy_i_ostatni("PHP"))
+
+print("Oryginalny ciąg: Python")
+print("Usunięcie pierwszego i ostatniego elementu:", usun_pierwszy_i_ostatni("Python"))
+
+print("Oryginalny ciąg: JavaScript")
+print("Usunięcie pierwszego i ostatniego elementu:", usun_pierwszy_i_ostatni("JavaScript"))
