9917b93383b89a5b232f79697cde4a14feca52ec
damiankiwi <kosipraca@gmail.com>
2024-01-12 02:20:20 +0100

    add basic

---
+++ 18/126.py
@@ -0,0 +1,11 @@
+def sortuj_litery(ciag):
+    return ''.join(sorted(ciag))
+
+print("Oryginalny ciąg: PHP")
+print("Posortuj litery ciągu:", sortuj_litery("PHP"))
+
+print("Oryginalny ciąg: javascript")
+print("Posortuj litery ciągu:", sortuj_litery("javascript"))
+
+print("Oryginalny ciąg: python")
+print("Posortuj litery ciągu:", sortuj_litery("python"))
