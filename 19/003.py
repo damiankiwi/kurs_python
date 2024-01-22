afb301417ecc2c846a0720a6ecf571b0392f81f4
damiankiwi <kosipraca@gmail.com>
2024-01-22 22:00:16 +0100

    add basic

---
+++ 19/003.py
@@ -0,0 +1,7 @@
+def sprawdz_warunki(liczba):
+    return liczba > 4**4 and liczba % 34 == 4
+
+print(sprawdz_warunki(922))
+print(sprawdz_warunki(914))
+print(sprawdz_warunki(854))
+print(sprawdz_warunki(854))
