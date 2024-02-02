3d9d988cfa9d2c0a57285ae8013b306a7892fa8f
damiankiwi <kosipraca@gmail.com>
2024-02-02 20:53:49 +0100

    add basic

---
+++ 19/024.py
@@ -0,0 +1,13 @@
+def xor_binarnych_napisow(napis1, napis2):
+    liczba1 = int(napis1, 2)
+    liczba2 = int(napis2, 2)
+    wynik = liczba1 ^ liczba2
+    return bin(wynik)
+
+input1 = ['0001', '1011']
+output1 = xor_binarnych_napisow(input1[0], input1[1])
+print(f"Wejście:\n{input1}\nWyjście:\n{output1}")
+
+input2 = ['100011101100001', '100101100101110']
+output2 = xor_binarnych_napisow(input2[0], input2[1])
+print(f"\nWejście:\n{input2}\nWyjście:\n{output2}")
