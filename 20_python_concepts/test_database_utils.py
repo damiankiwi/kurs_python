a88505f779d282be56c581629e50532929876671
damiankiwi <kosipraca@gmail.com>
2024-03-17 00:55:37 +0100

    add

--- 20_python_concepts/test_database_utils.py
+++ 20_python_concepts/test_database_utils.py
@@ -1,15 +1,20 @@
+import sys
 import unittest
 from unittest.mock import patch, MagicMock
-import your_module
+
+# Dodaj ścieżkę do katalogu zawierającego moduł database_utils
+sys.path.append('C:\\Python\\Python_Damian\\20_python_concepts')
+
+import database_utils
 
 class TestDatabaseConnection(unittest.TestCase):
 
-    @patch('your_module.connect_to_database')
+    @patch('database_utils.connect_to_database')
     def test_successful_connection(self, mock_connect):
         mock_connection = MagicMock()
         mock_connect.return_value = mock_connection
 
-        result = your_module.connect_to_database()
+        result = database_utils.connect_to_database()
 
         self.assertEqual(result, mock_connection)
 
