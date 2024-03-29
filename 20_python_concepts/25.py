c74cbdf17955ebffdcef51ef93abb954338af9f8
damiankiwi <kosipraca@gmail.com>
2024-03-29 23:09:11 +0100

    add

---
+++ 20_python_concepts/25.py
@@ -0,0 +1,43 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, root, key):
+        if root is None:
+            return TreeNode(key)
+        if key < root.key:
+            root.left = self._insert_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._insert_recursive(root.right, key)
+        return root
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, root, key):
+        if root is None or root.key == key:
+            return root
+        if key < root.key:
+            return self._search_recursive(root.left, key)
+        return self._search_recursive(root.right, key)
+
+bst = BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(20)
+bst.insert(40)
+bst.insert(70)
+bst.insert(60)
+bst.insert(80)
+
+print(bst.search(20))
+print(bst.search(90))
