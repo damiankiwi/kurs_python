8d31e66d431e11f567e62248591bba1c0314ebed
damiankiwi <kosipraca@gmail.com>
2024-03-29 23:11:36 +0100

    add

---
+++ 20_python_concepts/26.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Cannot pop from an empty stack")
+        return self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Cannot peek at an empty stack")
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("Popping elements from the stack:")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
