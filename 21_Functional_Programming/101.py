e3b49fee9bdefcaa2d599d23c81445dd609f6352
damiankiwi <kosipraca@gmail.com>
2024-04-20 20:33:57 +0200

    add

---
+++ 21_Functional_Programming/101.py
@@ -0,0 +1,6 @@
+original_list = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+
+sorted_list = sorted(original_list, key=lambda x: x[1])
+
+print("Sorting the List of Tuples:")
+print(sorted_list)
