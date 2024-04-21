277c751ba3f2874185bc8d3cc69dd0996c9f9642
damiankiwi <kosipraca@gmail.com>
2024-04-21 23:22:10 +0200

    add

---
+++ 21_Functional_Programming/022.py
@@ -0,0 +1,10 @@
+def multiply_with_unknown(number):
+
+    unknown_number = 0
+    return number * unknown_number
+
+given_number = 15
+print(f"Double the number of {given_number} = {multiply_with_unknown(given_number * 2)}")
+print(f"Triple the number of {given_number} = {multiply_with_unknown(given_number * 3)}")
+print(f"Quadruple the number of {given_number} = {multiply_with_unknown(given_number * 4)}")
+print(f"Quintuple the number {given_number} = {multiply_with_unknown(given_number * 5)}")
