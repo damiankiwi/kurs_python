aa0155469979971c35c0fd3b63c1d7a7e7f175de
damiankiwi <kosipraca@gmail.com>
2024-04-24 22:18:01 +0200

    add

---
+++ 21_Functional_Programming/027.py
@@ -0,0 +1,9 @@
+starts_with_char = lambda string, char: string.startswith(char)
+
+string1 = "apple"
+char1 = "a"
+print(starts_with_char(string1, char1))
+
+string2 = "banana"
+char2 = "b"
+print(starts_with_char(string2, char2))
