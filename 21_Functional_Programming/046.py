5857e22baf13876cc944bc244fe5931147d285ae
damiankiwi <kosipraca@gmail.com>
2024-05-07 22:12:12 +0200

    add

---
+++ 21_Functional_Programming/046.py
@@ -0,0 +1,11 @@
+original_list = [[0], [1, 3], [5, 7], [9, 11], [13, 15, 17]]
+
+max_length_list = max(original_list, key=lambda x: len(x))
+min_length_list = min(original_list, key=lambda x: len(x))
+
+print("Original list:")
+print(original_list)
+print("List with maximum length of lists:")
+print((len(max_length_list), max_length_list))
+print("List with minimum length of lists:")
+print((len(min_length_list), min_length_list))
