a311937fa212fa62ded50cea4ef114b51c48ea46
damiankiwi <kosipraca@gmail.com>
2024-05-19 02:06:51 +0200

    add

---
+++ 21_Functional_Programming/061.py
@@ -0,0 +1,8 @@
+original_list = ['Red', 'Green', 'Blue', 'White', 'Black']
+
+reversed_list = list(map(lambda x: x[::-1], original_list))
+
+print("Original list:")
+print(original_list)
+print("Reverse strings of the said given list:")
+print(reversed_list)
