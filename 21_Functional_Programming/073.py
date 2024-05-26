01718c70d6110939fc42ecb882bad33d43de96c7
damiankiwi <kosipraca@gmail.com>
2024-05-26 21:50:54 +0200

    add

---
+++ 21_Functional_Programming/073.py
@@ -0,0 +1,8 @@
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+triple_numbers = list(map(lambda x: x * 3, original_list))
+
+print("Original list of integers:")
+print(original_list)
+print("Triple all numbers in the said list:")
+print(triple_numbers)
