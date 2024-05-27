0293a7c32470fe1602485758e1d18f107c68f72e
damiankiwi <kosipraca@gmail.com>
2024-05-27 22:40:15 +0200

    add

---
+++ 21_Functional_Programming/075.py
@@ -0,0 +1,8 @@
+original_list = ['red', 'green', 'blue', 'white', 'black']
+
+listify_strings = list(map(list, original_list))
+
+print("Original list of strings:")
+print(original_list)
+print("Listify the list of given strings individually:")
+print(listify_strings)
