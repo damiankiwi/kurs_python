999991e6daeb4403d60f7481491996fb3a55e91e
damiankiwi <kosipraca@gmail.com>
2024-05-27 22:40:31 +0200

    add

---
+++ 21_Functional_Programming/077.py
@@ -0,0 +1,9 @@
+# zadanie 5
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+squared_list = list(map(lambda x: x ** 2, original_list))
+
+print("Original list of integers:")
+print(original_list)
+print("Square the elements of the said list:")
+print(squared_list)
