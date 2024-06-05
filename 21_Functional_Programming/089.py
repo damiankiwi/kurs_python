bf38ba1af1a4cae0a0aad36a00253474dd2e6b48
damiankiwi <kosipraca@gmail.com>
2024-06-06 00:01:16 +0200

    add

---
+++ 21_Functional_Programming/089.py
@@ -0,0 +1,9 @@
+def tuples_to_strings(list_of_tuples):
+    list_of_strings = list(map(lambda t: ''.join(map(str, t)), list_of_tuples))
+    return list_of_strings
+
+list_of_tuples = [(1, 2), (3, 4), (5, 6), ('a', 'b')]
+
+result = tuples_to_strings(list_of_tuples)
+
+print("List of Strings:", result)
