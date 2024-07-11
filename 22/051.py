2f9038d465860911ba7ca3cb5b1af077610872db
damiankiwi <kosipraca@gmail.com>
2024-07-12 00:25:31 +0200

    add

---
+++ 22/051.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+non_prime_numbers = list(filter(lambda x: not is_prime(x), numbers))
+
+print("Non-prime numbers in the list:")
+print(non_prime_numbers)
