fe1c72f544ac600fdb10ce9e1af451d601aadb0e
damiankiwi <kosipraca@gmail.com>
2024-07-17 22:04:04 +0200

    add

---
+++ 23/007.py
@@ -0,0 +1,11 @@
+from datetime import date, timedelta
+
+today = date.today()
+
+yesterday = today - timedelta(days=1)
+
+tomorrow = today + timedelta(days=1)
+
+print(f"Yesterday : {yesterday}")
+print(f"Today : {today}")
+print(f"Tomorrow : {tomorrow}")
