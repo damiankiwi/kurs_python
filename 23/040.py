e4cf61b3fbb95eb685579cfbee0f202c6a45a91d
damiankiwi <kosipraca@gmail.com>
2024-08-15 21:46:31 +0200

    add

---
+++ 23/040.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+current_datetime = datetime.now()
+
+print("Current date and time:", current_datetime)
+print("Year:", current_datetime.year)
+print("Month:", current_datetime.month)
+print("Day:", current_datetime.day)
+print("Hour:", current_datetime.hour)
+print("Minute:", current_datetime.minute)
+print("Second:", current_datetime.second)
+print("Microsecond:", current_datetime.microsecond)
