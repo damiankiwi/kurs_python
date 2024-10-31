c0719cf123303144aedbd3471fc22ea056e64902
damiankiwi <kosipraca@gmail.com>
2024-10-31 01:12:19 +0100

    add

---
+++ 24/064.py
@@ -0,0 +1,11 @@
+import csv
+fields = []
+rows = []
+with open('departments.csv', newline='') as csvfile:
+ data = csv.reader(csvfile, delimiter=' ', quotechar=',')
+ fields = next(data)
+ for row in data:
+   print(', '.join(row))
+print("\nTotal no. of rows: %d"%(data.line_num))
+print('Field names are:')
+print(', '.join(field for field in fields))
