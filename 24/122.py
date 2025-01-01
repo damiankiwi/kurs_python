ca6eac436e8986efad6c7f16f77ab58137fa6d43
damiankiwi <kosipraca@gmail.com>
2025-01-01 22:44:57 +0100

    add

---
+++ 24/122.py
@@ -0,0 +1,11 @@
+from re import sub
+
+def snake_case(s):
+    return '-'.join(
+        sub(r"(\s|_|-)+", " ",
+            sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
+                lambda mo: ' ' + mo.group(0).lower(), s)).split())
+
+print(snake_case('JavaScript'))
+print(snake_case('GDScript'))
+print(snake_case('BTW...what *do* you call that naming style? snake_case? '))
