e1ed58f3f944f59595ee33105b0ca79c34e4ddef
damiankiwi <kosipraca@gmail.com>
2025-03-17 22:29:30 +0100

    add

---
+++ 24/185.py
@@ -0,0 +1,6 @@
+import heapq as hq
+nums_list = [25, 35, 22, 85, 14, 65, 75, 22, 58]
+print("Original list:")
+print(nums_list)
+largest_nums = hq.nlargest(3, nums_list)
+print("\nThree largest numbers are:", largest_nums)
