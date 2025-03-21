b1844d70eabffea8e3d57a7141578fba64c5b48a
damiankiwi <kosipraca@gmail.com>
2025-03-21 15:01:06 +0100

    add

---
+++ 24/191.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution(object):
+    def find_Kth_Largest(self, nums, k):
+
+        h = []
+        for e in nums:
+            heapq.heappush(h, (-e, e))
+        for i in range(k):
+            w, e = heapq.heappop(h)
+            if i == k - 1:
+                return e
+
+arr_nums = [12, 14, 9, 50, 61, 41]
+s = Solution()
+result = s.find_Kth_Largest(arr_nums, 3)
+print("Third largest element:",result)
+result = s.find_Kth_Largest(arr_nums, 2)
+print("\nSecond largest element:",result)
+result = s.find_Kth_Largest(arr_nums, 5)
+print("\nFifth largest element:",result)
