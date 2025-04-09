ffbab6bd89d1ff308aa69abd76ecbd880bad355e
damiankiwi <kosipraca@gmail.com>
2025-04-09 22:00:34 +0200

    add

---
+++ 24/207.py
@@ -0,0 +1,13 @@
+import heapq
+heap = []
+heapq.heappush(heap, ('V', 3))
+heapq.heappush(heap, ('V', 2))
+heapq.heappush(heap, ('V', 1))
+print("Items in the heap:")
+for a in heap:
+	print(a)
+print("----------------------")
+print("Using heappushpop push item on the heap and return the smallest item.")
+heapq.heappushpop(heap, ('V', 6))
+for a in heap:
+	print(a)
