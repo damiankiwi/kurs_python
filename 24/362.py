bb5fba173b6bef845dfd7a50241eeba493123e9c
damiankiwi <kosipraca@gmail.com>
2025-12-02 00:14:06 +0100

    add

---
+++ 24/362.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+path = 'g:\\testpath\\'
+print('Path Name ({}):'.format(path))
+print('Size:', stat_info.st_size)
+print('Permissions:', oct(stat_info.st_mode))
+print('Owner:', stat_info.st_uid)
+print('Device:', stat_info.st_dev)
+print('Created     :', time.ctime(stat_info.st_ctime))
+print('Last modified:', time.ctime(stat_info.st_mtime))
+print('Last accessed:', time.ctime(stat_info.st_atime))
